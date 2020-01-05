91f1c76ddbba09b710084648c9e1d0a0d967301f
Sangmoo <first_1st@naver.com>
2020-01-06 00:32:28 +0900

    Custom User Model

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Using List
 
-- pipenv
-- python 3.6.7
-- django 2.2.5
+- Pipenv
+- Python 3.6.7
+- Django 2.2.5
+- Pillow
--- users/models.py
+++ users/models.py
@@ -4,4 +4,19 @@ from django.db import models
 # Import Model
 class User(AbstractUser):
 
-    pass
+    """ Custom User Model """  # Class Info.
+
+    GENDER_MALE = "male"  # Constant
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    # Tuple
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    bio = models.TextField(default="", blank=True)
