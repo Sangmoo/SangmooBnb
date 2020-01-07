5f80b2819ab6d4a95ec7bfadb7de1091f322a635
Sangmoo <first_1st@naver.com>
2020-01-07 23:46:26 +0900

    Finish User Model

--- users/models.py
+++ users/models.py
@@ -17,6 +17,32 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGAUGE_ENGLISH = "en"
+    LANGAUGE_KOREAN = "kr"
+
+    LANGAUGE_CHOICES = (
+        (LANGAUGE_ENGLISH, "English"),
+        (LANGAUGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
     avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    langauge = models.CharField(
+        choices=LANGAUGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
