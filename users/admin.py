9c1efecb46036c7a960f2649aa7fa35736e51c28
Sangmoo <first_1st@naver.com>
2020-01-09 23:17:24 +0900

    Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from . import models
 
-
+# Decorator # admin.site.register(models.User, CustomUserAdmin)
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "langauge", "currency", "superhost")
+    list_filter = ("langauge", "currency", "superhost")
