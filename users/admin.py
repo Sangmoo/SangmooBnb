98367e2001f674caab16e0a41611ad46be25395f
Sangmoo <first_1st@naver.com>
2020-03-22 23:24:42 +0900

    Rewiew Admin and Room Average

--- users/admin.py
+++ users/admin.py
@@ -25,3 +25,17 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
