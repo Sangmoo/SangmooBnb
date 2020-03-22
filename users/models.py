a793d9b2ffc6b9bdbf8884b72a1dd1d03527c092
Sangmoo <first_1st@naver.com>
2020-03-23 00:36:07 +0900

    Reservations Admin, Fix reservations-admin list_filter delete to second parameter

--- users/models.py
+++ users/models.py
@@ -1,42 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Import Model
+
 class User(AbstractUser):
 
-    """ Custom User Model """  # Class Info.
+    """ Custom User Model """
 
-    GENDER_MALE = "male"  # Constant
+    GENDER_MALE = "male"
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
 
-    # Tuple
     GENDER_CHOICES = (
-        (GENDER_MALE, "Male"),
-        (GENDER_FEMALE, "Female"),
-        (GENDER_OTHER, "Other"),
+        (GENDER_MALE, ("Male")),
+        (GENDER_FEMALE, ("Female")),
+        (GENDER_OTHER, ("Other")),
     )
 
-    LANGAUGE_ENGLISH = "en"
-    LANGAUGE_KOREAN = "kr"
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
 
-    LANGAUGE_CHOICES = (
-        (LANGAUGE_ENGLISH, "English"),
-        (LANGAUGE_KOREAN, "Korean"),
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, ("English")),
+        (LANGUAGE_KOREAN, ("Korean")),
     )
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
 
-    CURRENCY_CHOICES = (
-        (CURRENCY_USD, "USD"),
-        (CURRENCY_KRW, "KRW"),
-    )
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
     avatar = models.ImageField(blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(null=True)
-    langauge = models.CharField(choices=LANGAUGE_CHOICES, max_length=2, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
