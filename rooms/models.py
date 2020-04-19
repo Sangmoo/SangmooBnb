559f06bc035b2303a4cf1bcff1fd3b5a5623053f
Sangmoo <first_1st@naver.com>
2020-04-20 01:24:39 +0900

    django_seed module install

--- rooms/models.py
+++ rooms/models.py
@@ -99,7 +99,9 @@ class Room(core_models.TimeStampedModel):
         all_reviews = self.reviews.all()
         all_ratings = 0
         if len(all_reviews) > 0:
-            for review in all_reviews:
-                all_ratings += review.rating_average()
-            all_ratings += review.rating_average()
+            if len(all_reviews) > 0:
+                for review in all_reviews:
+                    all_ratings += review.rating_average()
+                return all_ratings / len(all_reviews)
+            return 0
         return all_ratings / len(all_reviews)
